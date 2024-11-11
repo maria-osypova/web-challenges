d2b44ece284539324e3749f601417a36d4b1147f
Maria Osypova <mosipova1998@gmail.com>
2024-11-11 19:02:52 +0100

    add couple of solutions

---
+++ js-functions/js-functions_greeting-page/js/index.js
@@ -0,0 +1,54 @@
+console.clear();
+
+/*
+Change the contents of this page depending on the current day and time.
+
+- Write a function getGreeting that returns a different greeting depending on the current time:
+  - 6 - 12: returns "Good Morning"
+  - 13 - 18: returns "Good Afternoon"
+  - 19 - 22: returns "Good Evening"
+  - 23 - 5: returns "Good Night"
+
+(HINT: you can get the current time: new Date().getHours() )
+
+- Write a function getDayColor that returns a different color depending on the current weekday:
+  - monday: "darkgray"
+  - tuesday - friday: "lightblue"
+  - saturday - sunday: "hotpink"
+
+(HINT: you can get the current weekday: new Date().getDay() )
+
+*/
+
+const display = document.querySelector('[data-js="display"]');
+
+function getGreeting() {
+  const currentHour = new Date().getHours();
+  if (currentHour >= 6 && currentHour <= 12) {
+    return "Good Morning";
+  } else if (currentHour >= 13 && currentHour <= 18) {
+    return "Good Afternoon";
+  } else if (currentHour >= 19 && currentHour <= 22) {
+    return "Good Evening";
+  } else {
+    return "Good Night";
+  }
+}
+
+const currentDay = new Date().getDay();
+
+console.log("day:", currentDay);
+
+function getDayColor() {
+  const currentDay = new Date().getDay();
+  if (currentDay === 1) {
+    return "darkgray";
+  } else if (currentDay >= 2 && currentDay <= 5) {
+    return "lightblue";
+  } else {
+    return "hotpink";
+  }
+}
+
+display.textContent = getGreeting();
+document.body.style.backgroundColor = getDayColor();
