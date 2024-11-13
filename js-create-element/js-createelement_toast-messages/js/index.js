0d3487cc86318fa2ab6afbb1d6931d9b32bd54b4
Maria Osypova <mosipova1998@gmail.com>
2024-11-13 16:01:29 +0100

    add solution to toast

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,13 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+addButton.addEventListener("click", (event) => {
+  const newToast = document.createElement("li");
+  newToast.classList.add("toast-container__message");
+  newToast.textContent = "I'm a toast message.";
+  toastContainer.appendChild(newToast);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
