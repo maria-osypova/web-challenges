e5bc991a33e2888af45c1ec3ca877f22ff829001
Maria Osypova <mosipova1998@gmail.com>
2024-11-19 13:11:29 +0100

    add solution to star-console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,11 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.results;
+  console.log("Fetched data:", results[2].eye_color);
+}
 
 fetchData();
