811deac2ddf642b6d246bc497cf4e69cb03064f0
Maria Osypova <mosipova1998@gmail.com>
2024-11-26 17:01:24 +0100

    add solution to counter

--- react-state-1/react-state-1_counter/src/App.jsx
+++ react-state-1/react-state-1_counter/src/App.jsx
@@ -1,26 +1,24 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
+  function addOne() {
+    setCount(count + 1);
+  }
+  function subtrackOne() {
+    setCount(count - 1);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button__subtrack" onClick={subtrackOne}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button__add" onClick={addOne}>
           +
         </button>
       </div>
