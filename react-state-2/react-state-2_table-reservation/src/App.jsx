95df99451d9d6452cbc96ccbb757000b059bfc6a
Maria Osypova <mosipova1998@gmail.com>
2024-11-27 19:40:57 +0100

    add solution to reservation

--- react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,23 @@
 import Counter from "./components/Counter";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleAdd() {
+    setPeople(people + 1);
+  }
+
+  function handleDelete() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onAdd={handleAdd} onDelete={handleDelete} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
