aae8a4f29b7c56863e4a65716588d199f9c80f0e
Maria Osypova <mosipova1998@gmail.com>
2024-11-27 12:17:30 +0100

    add solution to users

--- react-with-arrays/react-with-arrays_users/src/App.jsx
+++ react-with-arrays/react-with-arrays_users/src/App.jsx
@@ -9,7 +9,13 @@ export default function App() {
     <main className="app">
       <Title text="ClientBoard" />
       <div className="app__card-grid">
-        <Card user={USERS[0]} />
+        <ul>
+          {USERS.map((user) => (
+            <li key={user.id}>
+              <Card user={user} />
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
