25bb39188e74a7ea16e47472bf50045093928919
Maria Osypova <mosipova1998@gmail.com>
2024-12-10 13:32:21 +0100

    add solution to people

--- js-unit-testing/js-unit-testing_people/index.test.js
+++ js-unit-testing/js-unit-testing_people/index.test.js
@@ -48,13 +48,44 @@ test("getFirstNames() returns an array with the first names", () => {
   expect(result).toEqual(["Jane", "John", "Max", "Erika", "Luke"]);
 });
 
-test('getFullNames() returns an array with full names as strings, e.g. "Jane Doe"', () => {});
+test('getFullNames() returns an array with full names as strings, e.g. "Jane Doe"', () => {
+  const result = getFullNames(people);
+  expect(result).toEqual([
+    "Jane Doe",
+    "John Doe",
+    "Max Mustermann",
+    "Erika Musterfrau",
+    "Luke Skywalker",
+  ]);
+});
 
-test('getNameAndAge() returns an array of formatted strings like this: "Skywalker (23)"', () => {});
+test('getNameAndAge() returns an array of formatted strings like this: "Luke (23)"', () => {
+  const result = getNameAndAge(people);
+  expect(result).toEqual([
+    "Jane (21)",
+    "John (18)",
+    "Max (32)",
+    "Erika (38)",
+    "Luke (23)",
+  ]);
+});
 
-test("getPeopleByAge() returns an array of people of a certain age", () => {});
+test("getPeopleByAge() returns an array of people of a certain age", () => {
+  const result = getPeopleByAge(people, 18);
+  expect(result).toEqual([
+    {
+      id: 2,
+      firstName: "John",
+      lastName: "Doe",
+      age: 18,
+    },
+  ]);
+});
 
-test('getPeopleNamesOlderThan() returns an array of people full names older than a certain age, e.g. "Luke Skywalker"', () => {});
+test('getPeopleNamesOlderThan() returns an array of people full names older than a certain age, e.g. "Luke Skywalker"', () => {
+  const result = getPeopleNamesOlderThan(people, 37);
+  expect(result).toEqual(["Erika Musterfrau"]);
+});
 
 test("getPeopleByLastName() returns an array of people of a certain lastName", () => {});
 
